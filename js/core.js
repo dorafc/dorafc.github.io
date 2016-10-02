5fc4dcf271e2dc6d41611c5c15440cd2a1ae8092
Dora Fraeman <dorafraeman@gmail.com>
2016-10-02 16:44:00 -0400

    add work section, other updates

--- js/core.js
+++ js/core.js
@@ -1,3 +1,22 @@
 $('.block').hover(function() {
   $(this).addClass('flip');
 });
+
+//modal
+$(function() {
+  $(".modal-state").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
